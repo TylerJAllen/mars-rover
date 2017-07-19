7d5dd6642ffb13a5902b142f2eaa17528c5713aa
Tyler Allen <mynameistylerallen@gmail.com>
2017-07-18 20:41:43 -0700

    create mars rover project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { PhotosListComponent } from './photos-list/photos-list.component';
+import { RoverFormComponent } from './rover-form/rover-form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = '👽🚀🍑';
 }
